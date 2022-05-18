1fa1ff163db6a0783e69ce00da7a93b01f0ca9d0
Antoni Sieminski <63879863+asieminski@users.noreply.github.com>
2022-05-18 16:30:47 +0200

    change to execute specific lines as su

--- sh_install.sh
+++ sh_install.sh
@@ -2,15 +2,15 @@
 # License: GNU GPLv3
 # Designed for Pop_OS! 22.04
 # Run from your home directory (cd ~) and as root (sudo bash sh_install.sh)
-apt update
-apt upgrade -y
+sudo apt update
+sudo apt upgrade -y
 # Overcomes "dpkg was interrupted, you must manually run 'sudo dpkg --configure -a' to correct the problem" error
-sudo dpkg --configure -a
+# sudo dpkg --configure -a
 
 
 # Download and install miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.11.0-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p miniconda
+sudo bash miniconda.sh -b -p miniconda
 rm miniconda.sh
 miniconda/bin/conda init
 miniconda/bin/conda config --add channels conda-forge
@@ -18,36 +18,36 @@ miniconda/bin/conda config --add channels conda-forge
 
 # Install base R (https://cloud.r-project.org/bin/linux/ubuntu/)
 # update indices
-apt update -qq
+sudo apt update -qq
 # install two helper packages we need
-apt install -y software-properties-common dirmngr 
+sudo apt install -y software-properties-common dirmngr 
 # add the signing key (by Michael Rutter) for these repos
 # To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
 # Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 # add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
-add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/" -y
-apt update
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/" -y
+sudo apt update
 # Install R
-apt install r-base r-base-dev -y
+sudo apt install r-base r-base-dev -y
 # Removed these from install because they broke things 
 # echo "deb https://ppa.launchpadcontent.net/c2d4u.team/c2d4u4.0+/ubuntu/ focal main" > /etc/apt/sources.list.d/c2d4u_team-ubuntu-c2d4u4_0_-jammy.list
 # apt update
 
 # Install R dependencies (https://blog.zenggyu.com/en/post/2018-01-29/installing-r-r-packages-e-g-tidyverse-and-rstudio-on-ubuntu-linux/)
 # required by tidyverse
-apt install -y libcurl4-openssl-dev libssl-dev libxml2-dev
+sudo apt install -y libcurl4-openssl-dev libssl-dev libxml2-dev
 # required by r-base-core
-apt install -y zip unzip libpaper-utils xdg-utils libbz2-1.0 libc6 libcairo2 libcurl4 libglib2.0-0 libgomp1 libicu70 libjpeg8 liblzma5 libpango-1.0-0 libpangocairo-1.0-0 libpcre2-8-0 libpng16-16 libreadline8 libtcl8.6 libtiff5 libtirpc3 libtk8.6 libx11-6 libxt6 zlib1g ucf ca-certificates libcurl4-gnutls-dev
+sudo apt install -y zip unzip libpaper-utils xdg-utils libbz2-1.0 libc6 libcairo2 libcurl4 libglib2.0-0 libgomp1 libicu70 libjpeg8 liblzma5 libpango-1.0-0 libpangocairo-1.0-0 libpcre2-8-0 libpng16-16 libreadline8 libtcl8.6 libtiff5 libtirpc3 libtk8.6 libx11-6 libxt6 zlib1g ucf ca-certificates libcurl4-gnutls-dev
 # required by r-base-dev
-apt install -y build-essential gcc g++ gfortran libatlas-base-dev libncurses5-dev libreadline-dev libjpeg-dev libpcre2-dev libpcre3-dev libpng-dev zlib1g-dev libbz2-dev liblzma-dev libicu-dev xauth pkg-config
+sudo apt install -y build-essential gcc g++ gfortran libatlas-base-dev libncurses5-dev libreadline-dev libjpeg-dev libpcre2-dev libpcre3-dev libpng-dev zlib1g-dev libbz2-dev liblzma-dev libicu-dev xauth pkg-config
 # required for viewing graphics on-screen (configure R with `--with-x=no` if it is not needed)
-apt install -y xorg-dev
+sudo apt install -y xorg-dev
 # required by r-base-dev to build PDF help pages (optional; note that these dependencies are quite heavy, and since html help pages are already included, PDF ones are usually unnecessary)
-apt install -y texlive-base texlive-latex-base texlive-plain-generic texlive-fonts-recommended texlive-fonts-extra texlive-extra-utils texlive-latex-recommended texlive-latex-extra texinfo
+sudo apt install -y texlive-base texlive-latex-base texlive-plain-generic texlive-fonts-recommended texlive-fonts-extra texlive-extra-utils texlive-latex-recommended texlive-latex-extra texinfo
 
 
-apt install -y curl neovim make r-cran-tidyverse neofetch tree build-essential git ranger flatpak
+sudo apt install -y curl neovim make r-cran-tidyverse neofetch tree build-essential git ranger flatpak
 # I can install cmdstanr instead and languageserver isn't needed if I do :CocInstall coc-r-lsp:
 #R -e "install.packages(c('rstan', 'languageserver'))"
 
@@ -56,7 +56,7 @@ apt install -y curl neovim make r-cran-tidyverse neofetch tree build-essential g
 mkdir -p .config/nvim/
 wget -O .config/nvim/init.vim https://raw.githubusercontent.com/asieminski/ubuntu-installation-script/main/init.vim
 # vim-plug
-curl -fLo .local/share/nvim/site/autoload/plug.vim --create-dirs \
+curl -fLo ~/.var/app/io.neovim.nvim/data/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # Install nodejs for coc autocompletion plugin
 curl -sL install-node.vercel.app/lts | bash -s -- -y
