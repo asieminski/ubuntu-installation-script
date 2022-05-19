67df6e7b97d55d56605da0cda282cc096d727e46
Antoni Sieminski <63879863+asieminski@users.noreply.github.com>
2022-05-19 09:18:01 +0200

    add conda-forge to install

--- sh_install.sh
+++ sh_install.sh
@@ -64,7 +64,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 curl -sL install-node.vercel.app/lts | bash -s -- -y
 
 # neovim utilities install
-sudo miniconda/bin/conda install -y jedi pynvim radian
+sudo miniconda/bin/conda install -yc conda-forge jedi pynvim radian
 sudo R -e "install.packages('languageserver')"
 
 # Change bashrc setup
